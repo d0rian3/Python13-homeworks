a2e114143e0cc0b4ca2b5f70c89173ed322a05d3
d0rian3 <ggproebali16.0@gmail.com>
2025-03-18 18:48:01 +0100

    sd

---
+++ projects.py
@@ -0,0 +1,18 @@
+LOW = "1"
+MEDIUM = "2"
+HIGH = "3"
+PRIORITY = {
+    LOW: "low",
+    MEDIUM: "medium",
+    HIGH: "high"
+}
+NEW = "1"
+IN_PROGRESS = "2"
+DONE = "3"
+STATUS = {
+    NEW: "new",
+    IN_PROGRESS: "in progress",
+    DONE: "done"
+}
+
+def main():
